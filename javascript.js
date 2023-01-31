b961043b053862578ff101a4bcea1cf8c85119a2
Pablo Vincenty <emanuelvincenty@gmail.com>
2023-01-31 12:13:45 -0300

    Primer escritura en el repositorio

---
+++ javascript.js
@@ -0,0 +1,14 @@
+
+var toggle = document.getElementById('button-container');
+var body = document.querySelector('body');
+
+toggle.onclick = function () {
+    toggle.classList.toggle('active');
+    body.classList.toggle('active');
+}
+
+
+
+
+
+
